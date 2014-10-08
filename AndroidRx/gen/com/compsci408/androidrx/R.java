4a6a1d0e9305a5227cf741010eb5c49e15ef7de0
lemongod <wang.q.vincent@gmail.com>
2014-10-08 03:32:15 -0400

    Pull fix

--- AndroidRx/gen/com/compsci408/androidrx/R.java
+++ AndroidRx/gen/com/compsci408/androidrx/R.java
@@ -14,11 +14,11 @@ public final class R {
         public static final int title_animation=0x7f040002;
     }
     public static final class array {
-        public static final int account_types=0x7f080002;
-        public static final int med_list=0x7f080001;
+        public static final int account_types=0x7f070002;
+        public static final int med_list=0x7f070001;
         /**  For testing purposes only; remove when real data is available 
          */
-        public static final int patient_list=0x7f080000;
+        public static final int patient_list=0x7f070000;
     }
     public static final class attr {
     }
@@ -30,8 +30,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f070000;
-        public static final int activity_vertical_margin=0x7f070001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int custom_button=0x7f020000;
@@ -107,32 +107,32 @@ public final class R {
         public static final int patient_list=0x7f090004;
     }
     public static final class string {
-        public static final int action_logout=0x7f050003;
-        public static final int action_settings=0x7f050002;
-        public static final int action_sign_in=0x7f050010;
-        public static final int action_sign_in_short=0x7f050011;
-        public static final int add_med=0x7f050005;
-        public static final int androidrx=0x7f050004;
-        public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050015;
-        public static final int error_incorrect_password=0x7f050014;
-        public static final int error_invalid_password=0x7f050013;
-        public static final int error_invalid_username=0x7f050012;
-        public static final int hello_world=0x7f050001;
-        public static final int new_med=0x7f050006;
-        public static final int patient=0x7f050016;
-        public static final int prompt_password=0x7f05000f;
+        public static final int action_logout=0x7f060003;
+        public static final int action_settings=0x7f060002;
+        public static final int action_sign_in=0x7f060010;
+        public static final int action_sign_in_short=0x7f060011;
+        public static final int add_med=0x7f060005;
+        public static final int androidrx=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int error_field_required=0x7f060015;
+        public static final int error_incorrect_password=0x7f060014;
+        public static final int error_invalid_password=0x7f060013;
+        public static final int error_invalid_username=0x7f060012;
+        public static final int hello_world=0x7f060001;
+        public static final int new_med=0x7f060006;
+        public static final int patient=0x7f060016;
+        public static final int prompt_password=0x7f06000f;
         /**  Strings related to login 
          */
-        public static final int prompt_username=0x7f05000e;
-        public static final int provider=0x7f050017;
-        public static final int title_activity_login=0x7f05000d;
-        public static final int title_activity_main=0x7f050007;
-        public static final int title_activity_new_med=0x7f05000b;
-        public static final int title_activity_new_time=0x7f05000c;
-        public static final int title_activity_patient=0x7f05000a;
-        public static final int title_activity_patient_list=0x7f050009;
-        public static final int title_activity_splash=0x7f050008;
+        public static final int prompt_username=0x7f06000e;
+        public static final int provider=0x7f060017;
+        public static final int title_activity_login=0x7f06000d;
+        public static final int title_activity_main=0x7f060007;
+        public static final int title_activity_new_med=0x7f06000b;
+        public static final int title_activity_new_time=0x7f06000c;
+        public static final int title_activity_patient=0x7f06000a;
+        public static final int title_activity_patient_list=0x7f060009;
+        public static final int title_activity_splash=0x7f060008;
     }
     public static final class style {
         /** 
@@ -155,34 +155,12 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int App_ButtonStyle=0x7f060003;
-        public static final int App_EditTextStyle=0x7f060002;
+        public static final int AppTheme=0x7f080001;
+        public static final int App_ButtonStyle=0x7f080003;
+        public static final int App_EditTextStyle=0x7f080002;
     }
 }
